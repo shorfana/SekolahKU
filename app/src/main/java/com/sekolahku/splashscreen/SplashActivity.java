eb8681506de4b04594e4f76a01c12a4c4f2a0dae
Erzi Hutama Dwirama Putra <erzihutama@gmail.com>
2018-12-12 21:19:59 +0700

    update splash

---
+++ app/src/main/java/com/sekolahku/splashscreen/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.sekolahku.splashscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sekolahku.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
